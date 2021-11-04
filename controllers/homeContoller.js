9f5b39417a48fe1592b5bab06073cc9e2e901798
oppoalikhan <alik43239@gmail.com>
2021-11-03 23:53:39 -0700

    login
    login

--- controllers/homeContoller.js
+++ controllers/homeContoller.js
@@ -3,13 +3,31 @@ var connection  = require('../config/db.config');
 
 
 module.exports = {
-   indexView: async function (req, res,next) {
-      res.render('../views/layout.ejs');
-   },
+   // indexView: async function (req, res,next) {
+   //    res.render('../views/layout.ejs');
+   // },
    loginView: async function (req, res,next) {
-      res.render('../views/login.ejs');
-   
-}
+      res.render('../views/login.ejs');   
+},
+     indexView: async function (req,res,next){
+      var username = request.body.username;
+      var password = request.body.password;
+      if (username && password) {
+         connection.query('SELECT * FROM logUser WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+            if (results.length > 0) {
+               request.session.loggedin = true;
+               request.session.username = username;
+               response.redirect('/dashbaord');
+            } else {
+               response.send('Incorrect Username and/or Password!');
+            }			
+            response.end();
+         });
+      } else {
+         response.send('Please enter Username and Password!');
+         response.end();
+      }
+    }
 }
 
 
--- routes/home-route.js
+++ routes/home-route.js
@@ -3,12 +3,21 @@ const express =require('express');
 let userController = require('../controllers/homeContoller');
 const router = express.Router();
 
-router.get("/dashboard", function(req, res) {
-    userController.indexView(req,res);
-    });
+// router.get("/dashboard", function(req, res) {
+//     userController.indexView(req,res);
+//     });
      router.get("/", function(req, res) {
         userController.loginView(req,res);
         });
+       router.get("/dashbaord", function(req,res){
+          userController.indexView(req,res);
+          if (request.session.loggedin) {
+            response.render('../views/layout.ejs ');
+        } else {
+            response.send('Please login to view this page!');
+        }
+        response.end();
+       });
     
     module.exports =router;
 
